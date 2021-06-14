cae1e4d3305b7e4395e3539b289b09038ec80fca
Puneet-goel <puneetgoel016@gmail.com>
2021-06-14 17:21:54 +0530

    React Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './bootstrap-4.0.0/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
+import './bootstrap-4.0.0/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
