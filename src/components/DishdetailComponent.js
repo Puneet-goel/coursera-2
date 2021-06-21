ed21a0976b1b27c098295fb62a6bfea76be667e1
Puneet-goel <puneetgoel016@gmail.com>
2021-06-21 18:10:53 +0530

    Assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,73 @@
-import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, ModalHeader, ModalBody, Modal, Button, Label } from "reactstrap";
 import { Link } from 'react-router-dom';
+import { LocalForm, Control, Errors } from 'react-redux-form'
+
+const required = (val) => val && val.length ;
+const minLength = (len) => (val) => !(val) || (val.length>=3);
+const maxLength = (len) => (val) => !(val) || (val.length<=15);
+
+class CommentForm extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isModalOpen : false
+        }
+        this.toggleModal = this.toggleModal.bind(this);
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen : !this.state.isModalOpen 
+        });
+    };
+
+    render(){
+        return (
+            <div> 
+                <Button outline color="secondary" onClick={this.toggleModal} className="fa fa-pencil"> Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm>
+                            <Label htmlfor="rating" >Rating</Label>
+                            <Control.select model=".rating" name="rating" rows="6" className="col-xl">
+                                <option>1</option> 
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Control.select> 
+                            <Label htmlfor="author" >Your Name</Label>
+                            <Control.text model=".author" id="author" name="author" 
+                                placeholder="Your Name" 
+                                rows="6" 
+                                className="col-xl"
+                                validators = {{
+                                    required,minLength : minLength(3),maxLength : maxLength(15)
+                                }}
+                            />
+                            <Errors 
+                                className="text-danger"
+                                model=".author"
+                                show="touched"
+                                messages={{
+                                    required : "Required",
+                                    minLength :  "Must be greater than 2 characters",
+                                    maxLength : "Must be 15 characters or less"
+                                }}
+                            />
+                            <Label htmlFor="comment">Comment</Label> 
+                            <Control.textarea model=".comment" id="comment" name="comment" rows="6" className="col-xl"/> <br/>
+                        </LocalForm>
+                        <Button color="primary" type="submit">Submit</Button>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+    }
+}
 
 function RenderDish({dish}){
 	return(
@@ -30,6 +97,7 @@ function RenderComments({comments}){
             <ul className = "list-unstyled">
                 { x }
             </ul>
+            <CommentForm/>
         </div>  
     );
 }
