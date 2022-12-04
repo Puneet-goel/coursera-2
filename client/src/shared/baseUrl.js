a094c05873c2ba54d3268acdda6552be8ba0ccf8
Puneet Goel <puneetgoel016@gmail.com>
2022-12-04 21:07:50 +0530

    shift deployment to render

--- client/src/shared/baseUrl.js
+++ client/src/shared/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = "https://hotel-json-server-react.herokuapp.com/";
+export const baseUrl = 'https://hotel-json-server-react.onrender.com/';
+//export const herokuURL = "https://hotel-json-server-react.herokuapp.com/";
